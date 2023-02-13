ef3f9f9d6f0c8c0a57414a9bdc474f2a7f187237
Akshay <akshayarora1909@gmail.com>
2023-02-13 17:42:04 -0600

    table write

--- find_yourself_app/workflow/services/data_loader_db.py
+++ find_yourself_app/workflow/services/data_loader_db.py
@@ -5,6 +5,6 @@ engine = create_engine('postgresql://akshay:seguinbenn@0.0.0.0:5432/genome')
 conn=engine.connect()
 
 def data_loader_db(table_name,df):
-    df.to_sql(table_name, conn, if_exists='append', index=False, schema="public")
+    df.to_sql(table_name, conn, if_exists='replace', index=False, schema="public")
     status = "successfully loaded data into table :{} with rows:{}".format(table_name, df.shape[0])
     return status
