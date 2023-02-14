244d7152a7ff4027990ed2b13157870378e3e674
Akshay <akshayarora1909@gmail.com>
2023-02-13 23:12:34 -0600

    testing module

---
+++ find_yourself_app/workflow/data_pipeline_workflow.py
@@ -0,0 +1,43 @@
+from .services.data_loader_db import data_loader_db
+from .services.data_preprocessing import data_processor,data_processor_test
+from .services.get_classification_service import get_classification,get_classification_test
+from .services.reduce_dimension_service import reduce_dimension_service,reduce_dimension_service_test
+from .services.ohe_service import get_one_hot_encoding,get_one_hot_encoding_test
+import warnings
+warnings.filterwarnings(action="ignore",category=DeprecationWarning)
+
+def data_training_workflow():
+    genome_samples,genome_fact,genome_fact_pivot,genome_asinp = data_processor()
+    print("data_preprocessing done...! ")
+    ohe=get_one_hot_encoding(genome_fact_pivot)
+    print("one hot encoding done..!")
+    df_dim_transformed = reduce_dimension_service(genome_samples,ohe, algorithm='PCA', n_components=3)
+    print("dimensionality reduction done...! ")
+    get_classification(df_dim_transformed[["x", "y", "z"]].values, df_dim_transformed["super_pop"].values)
+    print("classification done...! ")
+    print("writing table :dim_reduce ")
+    data_loader_db("dim_reduce", df_dim_transformed)
+    print("writing table :genome_samples ")
+    data_loader_db("genome_samples", genome_samples)
+    print("writing table :genome_sample_fact ")
+    data_loader_db("genome_sample_fact", genome_fact)
+    print("writing table :genome_fact_pivot ")
+    data_loader_db("genome_fact_pivot", genome_fact_pivot)
+    print("writing table :genome_asinp ")
+    data_loader_db("genome_asinp", genome_asinp)
+
+def data_testing_workflow(genome_test):
+    genome_fact, genome_fact_pivot, genome_asinp=data_processor_test(genome_test)
+    print("data_preprocessing done...! ")
+    ohe=get_one_hot_encoding_test(genome_fact_pivot)
+    print("one hot encoding done..!")
+    dim_red=reduce_dimension_service_test(ohe,"PCA",genome_test)
+    print("dim reduction done..!")
+    print(dim_red.head(3))
+    test_class=get_classification_test(dim_red,genome_test)   
+    print("classfication done..!")
+    print(test_class)
+
+  
+
+    
--- find_yourself_app/workflow/services/reduce_dimension_service.py
+++ find_yourself_app/workflow/services/reduce_dimension_service.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import os
 from sklearn.decomposition import PCA
-from sklearn.preprocessing import OneHotEncoder
+from .utils import load_model,save_model
 import warnings
 warnings.filterwarnings(action="ignore",category=DeprecationWarning)
 
-def get_one_hot_encoding(df):
-    ncols = len(df.columns)
-    ohe = OneHotEncoder(categories=[range(4)] * ncols, sparse=False)
-    return ohe.fit_transform(df.values)
 
-
-def reduce_dimension_service(genome_samples,pivot_df, algorithm='PCA', n_components=1):
+def reduce_dimension_service(df,ohe, algorithm='PCA', n_components=1):
     """Reduce the dimensionality of the 55 AISNPs
     :param genome_fact_pivot:
     :param X: One-hot encoded 1kG 55 AISNPs.
@@ -24,16 +19,29 @@ def reduce_dimension_service(genome_samples,pivot_df, algorithm='PCA', n_compone
     """
     color_lookup={"EAS":"#FFBF00","AFR":"#40E0D0","AMR":"#DE3163","EUR":"#6495ED","SAS":"#AF7AC5"}
     super_pop_lookup={"EAS":"EAST ASIA","AFR":"AFRICA","AMR":"AMERICA","EUR":"EUROPE","SAS":"SOUTH ASIA"}
-    genome_dir="genome_data"
-    df_encoded = get_one_hot_encoding(pivot_df)
+    df_encoded = ohe
     if algorithm == 'PCA':
-        transformed = PCA(n_components=n_components).fit_transform(df_encoded)
+        pca_mod = PCA(n_components=n_components).fit(df_encoded)
+        save_model(pca_mod,"pca","pca")
+        transformed = pca_mod.transform(df_encoded)
     transformed_df=pd.DataFrame(transformed)
     transformed_df.columns=["x","y","z"]
-    pca_df=pd.concat([transformed_df,genome_samples],axis=1)
+    pca_df=pd.concat([transformed_df,df],axis=1)
     pca_df["color"]=pca_df["super_pop"].map(color_lookup)
     pca_df["pop_zone"]=pca_df["super_pop"].map(super_pop_lookup)
-    if not os.path.exists(genome_dir):
-      os.makedirs(genome_dir)
-    pca_df.to_csv(os.path.join(genome_dir,"dim_reduce.csv"),index=False)
     return pca_df
+
+def reduce_dimension_service_test(ohe,algorithm,genome_test):
+    color_lookup={"EAS":"#FFBF00","AFR":"#40E0D0","AMR":"#DE3163","EUR":"#6495ED","SAS":"#AF7AC5"}
+    super_pop_lookup={"EAS":"EAST ASIA","AFR":"AFRICA","AMR":"AMERICA","EUR":"EUROPE","SAS":"SOUTH ASIA"}
+    if algorithm=="PCA":
+        pca_mod=load_model("pca","pca")   
+        pca_transformed=pca_mod.transform(ohe)
+        transformed_df=pd.DataFrame(pca_transformed)
+        transformed_df.columns=["x","y","z"]
+        pca_df=pd.concat([transformed_df,genome_test],axis=1)
+        pca_df["color"]=pca_df["super_pop"].map(color_lookup)
+        pca_df["pop_zone"]=pca_df["super_pop"].map(super_pop_lookup)
+    return pca_df
+
+    
